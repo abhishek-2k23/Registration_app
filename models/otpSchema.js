7d61827d63e131ab82aabdbed52f5e80725144d7
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-07-12 20:43:04 +0530

    expire date modified

--- models/otpSchema.js
+++ models/otpSchema.js
@@ -13,7 +13,7 @@ const otpSchema = new mongoose.Schema({
     createdAt:{
         type : Date,
         default : Date.now(),
-        expires:5*60,
+        expires:Date.nowa()+5*60,
     }
 });
 
